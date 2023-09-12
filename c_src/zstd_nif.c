d74f8403f320cfd560b4a8afd92444c3270fdd51
Ricardo Azpeitia Pimentel <ricardo.azpeitia@nextroll.com>
2023-09-12 16:11:32 -0600

    Fix spaces

--- c_src/zstd_nif.c
+++ c_src/zstd_nif.c
@@ -405,11 +405,11 @@ static int zstd_on_upgrade(ErlNifEnv *env, void **priv, void **old, ERL_NIF_TERM
 }
 
 static ErlNifFunc nif_funcs[] = {
-  { "compress"                     , 2, zstd_nif_compress                   , ERL_DIRTY_JOB_CPU_BOUND },
-  { "decompress"                   , 1, zstd_nif_decompress                 , ERL_DIRTY_JOB_CPU_BOUND },
+  { "compress"                     , 2, zstd_nif_compress                  , ERL_DIRTY_JOB_CPU_BOUND },
+  { "decompress"                   , 1, zstd_nif_decompress                , ERL_DIRTY_JOB_CPU_BOUND },
 
-  { "new_compression_stream"      , 0, zstd_nif_new_compression_stream                                },
-  { "new_decompression_stream"    , 0, zstd_nif_new_decompression_stream                              },
+  { "new_compression_stream"      , 0, zstd_nif_new_compression_stream                               },
+  { "new_decompression_stream"    , 0, zstd_nif_new_decompression_stream                             },
 
   { "compression_stream_init"     , 1, zstd_nif_init_compression_stream    , ERL_DIRTY_JOB_CPU_BOUND },
   { "compression_stream_init"     , 2, zstd_nif_init_compression_stream    , ERL_DIRTY_JOB_CPU_BOUND },
