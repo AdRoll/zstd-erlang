160d0a357a6cb90f9d44d2df2571ba77a14c3000
Ricardo Azpeitia Pimentel <ricardo.azpeitia@nextroll.com>
2025-06-17 18:18:55 +0000

    Add window log param (#53)
    * Add window log param
    
    * Add checksum by default

--- test/zstd_tests.erl
+++ test/zstd_tests.erl
@@ -11,7 +11,7 @@ zstd_test() ->
 zstd_stream_test() ->
     Bin = << <<"A">> || _ <- lists:seq(1, 1024 * 1024) >>,
     CStream = zstd:new_compression_stream(),
-    ok = zstd:compression_stream_init(CStream),
+    ok = zstd:compression_stream_init(CStream, 5, 23),
     {ok, CompressionBin} = zstd:stream_compress(CStream, Bin),
     {ok, FlushBin} = zstd:stream_flush(CStream),
 
